73b80c4c09761fad69bd639e39cd1169631c508c
njitech <romeotagyen@gmail.com>
2026-07-08 22:44:16 -0400

    fix: semantic release

--- release.config.js
+++ release.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-    branches: "main",
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/git',
-      '@semantic-release/github',
-      ['@semantic-release/npm',
-             {
-             'npmPublish': false
-             }
-       ]
-    ]
- }
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
